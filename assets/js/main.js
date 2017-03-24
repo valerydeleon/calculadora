1e60e9995e4b444d448626f30505abab8bee74ed
Valeria de Leon <valery.deleon@gmail.com>
2017-03-24 11:51:56 -0600

    ya suma

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,58 @@
-var numero1 = 0;
-var numero2 = 0;
-var operacion;
+// var numero1 = 0;
+// var numero2 = 0;
+// var operacion;
+//
+// //recibir numeros
+// function mostrarNumero(numero){
+//   if(document.getElementById("resultado").value == "0" || numero1 == 0 ){
+//     numero1 = numero;
+//   } else{
+//     numero1 += numero;
+//   }
+//   document.getElementById("resultado").value = numero1;
+// }
 
-//recibir numeros
-function mostrarNumero(numero){
-  if(document.getElementById("resultado").value == "0" || numero1 == 0 ){
-    numero1 = numero;
-  } else{
-    numero1 += numero;
+
+
+
+
+
+
+
+//        *********  Este es el bueno  ************
+
+function mostrarNumero(valor){
+  var input =  document.getElementById("operacion")
+  input.value += valor;
+}
+
+function almacenar(){
+    var numeros =document.getElementById("operacion").value;
+    var respuesta = document.getElementById('resultado');
+    var almacen = numeros.split("+");
+    var suma= Number(almacen[0]) + Number(almacen [1]);
+    respuesta.value = suma;
   }
-  document.getElementById("resultado").value = numero1;
+
+function borrar(){
+  var inputC = document.getElementById("operacion")
+  inputC.value = "";
 }
 
 
 
-//
-// function mostrar(valor){
-//   var input =  document.getElementById("resultado")
-//   input.value += valor;
-// }
-//
-// function borrar(){
-//   var inputC = document.getElementById("resultado")
-//   inputC.value = "";
-// }
-//
+
+
+
+
+
+
+
+
 // function sumar(num1, num2){
 //
 // }
-//
+
 //
 // var num1 = parseInt(document.getElementById("btn1").value);
 // var num2 = parseInt(document.getElementById("btn2").value);
